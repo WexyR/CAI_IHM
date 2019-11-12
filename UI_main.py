e361033d3473f5aa186c353543b3861466d7ee74
ShinySilver <nicolas.louismarie@gmail.com>
2019-11-12 15:06:00 +0100

    Improved UI

--- UI_main.py
+++ UI_main.py
@@ -5,47 +5,30 @@ if sys.version_info.major == 2:
     import tkFileDialog as filedialog
 else:
     print(sys.version)
-    from tkinter import Tk,Frame,Button,Label,LabelFrame,Menu,Toplevel
+    from tkinter import Tk,Frame,Button,Label,LabelFrame,Menu,Toplevel,messagebox
     from tkinter.ttk import Notebook
     from tkinter import filedialog
 
 class UI_Main (Tk):
+    """This is the main UI class. It is responsible for the module linking."""
+
+    def on_closing(self):
+        if messagebox.askokcancel("Avertissement fermeture", "Voulez-vous vraiment quitter l'application?"):
+            self.destroy()
+
     def __init__(self):
         Tk.__init__(self)
 
         self.geometry("600x350")
         self.title("La leçon de piano - Beta Version - Do not distribute")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         ######################################################################
-        #                           Menus
+        #                           Menubar init
         ######################################################################
 
         menu_bar = Menu()
         self.config(menu = menu_bar)
-
-        file_menu = Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="Quitter")
-        menu_bar.add_cascade(label="Fichier", menu=file_menu)
-
-        edition_menu = Menu(menu_bar, tearoff=0)
-        edition_menu.add_command(label="Regenérer Tout")
-        edition_menu.add_command(label="Rétablir Défaut")
-        menu_bar.add_cascade(label="Edition", menu=edition_menu)
-
-        window_menu = Menu(menu_bar, tearoff=0)
-        window_submenu1 = Menu(window_menu, tearoff=0)
-        window_submenu1.add_command(label="Afficheur Onde")
-        window_submenu1.add_command(label="Afficheur Harmonique")
-        window_menu.add_cascade(label="Ouvrir Vue", menu=window_submenu1)
-        window_menu.add_command(label="Ouvrir Tout")
-        window_menu.add_separator()
-        window_menu.add_command(label="Rétablir défaut")
-        menu_bar.add_cascade(label="Fenêtre", menu=window_menu)
-
-        help_menu = Menu(menu_bar, tearoff=0)
-        help_menu.add_command(label="Support")
-        help_menu.add_command(label="Crédits")
-        menu_bar.add_cascade(label="Aide", menu=help_menu)
 
         subframe_menu = Notebook(self)
         subframe_menu.pack(fill="both", expand="yes")
@@ -70,18 +53,6 @@ class UI_Main (Tk):
 
         label2 = Label(frame2, text="Générer accords ici").pack()
 
-        ##########################
-
-        ######################################################################
-        #                         Fenêtre Affichage
-        ######################################################################
-
-        self.plotter_frame = Toplevel(self)
-        self.plotter_frame.geometry("400x200")
-        self.plotter_frame.disabled = 1
-        self.plotter_frame.title("Affichage des notes");
-
-        label3 = Label(self.plotter_frame, text="Affichage ici").pack()
 
         ######################################################################
         #                          Mode Clavier
@@ -92,6 +63,53 @@ class UI_Main (Tk):
         subframe_menu.add(frame4, text="Clavier")
 
         label4 = Label(frame4, text="Clavier ici").pack()
+
+        ######################################################################
+        #                         Fenêtre Affichage
+        ######################################################################
+
+        self.plotter_frame = Toplevel(self)
+        self.plotter_frame.geometry("400x200")
+        #self.plotter_frame.withdraw()
+        self.plotter_frame.title("Affichage des notes");
+
+        def on_plotter_frame_closing():
+            self.plotter_frame.withdraw()
+
+        self.plotter_frame.protocol("WM_DELETE_WINDOW", on_plotter_frame_closing)
+
+        label3 = Label(self.plotter_frame, text="Affichage ici").pack()
+
+        ######################################################################
+        #                          Menubar linking
+        ######################################################################
+
+        file_menu = Menu(menu_bar, tearoff=0)
+        file_menu.add_command(label="Quitter")
+        menu_bar.add_cascade(label="Fichier", menu=file_menu)
+
+        edition_menu = Menu(menu_bar, tearoff=0)
+        edition_menu.add_command(label="Regenérer Tout")
+        edition_menu.add_command(label="Rétablir Défaut")
+        menu_bar.add_cascade(label="Edition", menu=edition_menu)
+
+        window_menu = Menu(menu_bar, tearoff=0)
+        window_submenu1 = Menu(window_menu, tearoff=0)
+        window_submenu1.add_command(label="Afficheur Onde", command=self.plotter_frame.deiconify)
+        window_submenu1.add_command(label="Afficheur Harmonique")
+        window_menu.add_cascade(label="Ouvrir Vue", menu=window_submenu1)
+        window_menu.add_command(label="Ouvrir Tout")
+        window_menu.add_separator()
+        window_menu.add_command(label="Rétablir défaut")
+        menu_bar.add_cascade(label="Fenêtre", menu=window_menu)
+
+        help_menu = Menu(menu_bar, tearoff=0)
+        help_menu.add_command(label="Support")
+        help_menu.add_command(label="Crédits")
+        menu_bar.add_cascade(label="Aide", menu=help_menu)
+
+    def toggleToplevel(level):
+        self.plotter_frame.deiconify()
 
 if __name__ == "__main__":
     app = UI_Main()
