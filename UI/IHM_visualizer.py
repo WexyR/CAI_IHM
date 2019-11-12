70ec6a62fe2d81bbb366d96453dd71f5e15dc9aa
WexyR <romain.biannic2@gmail.com>
2019-11-12 23:25:47 +0100

    NoteSelector implemented model implementation left

--- UI/IHM_visualizer.py
+++ UI/IHM_visualizer.py
@@ -4,11 +4,11 @@ from observer import Subject, Observer
 import sys
 if sys.version_info.major == 2:
     print(sys.version)
-    from Tkinter import Tk,Frame, StringVar, OptionMenu, Checkbutton, Spinbox
+    from Tkinter import Tk,Frame, StringVar, OptionMenu, Checkbutton, Spinbox, Label, Button
     import tkFileDialog as filedialog
 else:
     print(sys.version)
-    from tkinter import Tk,Frame,StringVar, OptionMenu, Checkbutton, Spinbox
+    from tkinter import Tk,Frame,StringVar, OptionMenu, Checkbutton, Spinbox, Label, Button
     from tkinter import filedialog
 
 
@@ -20,11 +20,21 @@ class NoteSelector(Frame):
         self.model = model
 
 
-        # vcmd= (self.register(self._validate_spinbox), '%W', '%P')
+
+    def _validate_spinbox(self, S, P):
+        """[private] validation function of Spinbox widget"""
+        if S == '' or P == '': return True
+        result = False
+        if (S.isdigit()):
+            if(int(P)>=2 and int(P)<=5):
+                result = True
+        return result
+
 
 
 
     def create_UI(self):
+
         def sharpable(key, checkbox):
             if(key in "BE"):
                 checkbox.deselect()
@@ -34,17 +44,24 @@ class NoteSelector(Frame):
 
 
 
-        keys = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
-        v = StringVar()
-        v.set(keys[0])
-        is_sharp = Checkbutton(self,text="#")
-        om = OptionMenu(self, v, *keys, command=lambda value=v.get(), checkbox=is_sharp:sharpable(value, checkbox))
-        om.pack(side="left")
-        is_sharp.pack(side="left")
 
-        octave = Spinbox(self, from_=2, to=5)
-        octave.pack(side="left")
 
+        keys = tuple("ABCDEFG")
+        keyvar = StringVar()
+        keyvar.set(keys[0])
+        sharpvar = StringVar()
+        is_sharp = Checkbutton(self,text="#", variable=sharpvar, onvalue='#', offvalue='')
+        om = OptionMenu(self, keyvar, *keys, command=lambda value=keyvar.get(), checkbox=is_sharp:sharpable(value, checkbox))
+        om.grid(row=2, column=1)
+        is_sharp.grid(row=2, column=2)
+
+        Label(self, text="Octave 2-5").grid(row=1, column=3)
+
+        vcmd_spinbox = (self.register(self._validate_spinbox), '%S', '%P')
+        octave = Spinbox(self, from_=2, to=5, validate="all",validatecommand=vcmd_spinbox)
+        octave.grid(row=2, column=3)
+
+        Button(self, text="test", command=lambda k=keyvar, s=sharpvar:print(k.get() + s.get() + octave.get())).grid(row=2, column=4)
 
 
 
