32e60965c76f08c46a81d3276c415f618607b439
ShinySilver <nicolas.louismarie@gmail.com>
2019-12-01 18:32:57 +0100

    Fixed main_ui imports

--- piano.py
+++ piano.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
         print("This program must be executed using python 3.X You are currently using python " \
         +str(sys.version_info.major)+"."+str(sys.version_info.minor))
         sys.exit(0)
-    import Utils.wav_create_notes_from_frequencies_db
+    import Generation.wav_create_notes_from_frequencies_db
     from UI.main_ui import MainUI
     app = MainUI()
     app.mainloop()
